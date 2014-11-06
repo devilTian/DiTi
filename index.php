bab7fd357bfefb56898df0e5030a696ca382a849
tianye <tianye@DTPC>
2014-11-06 16:18:27 +0800

    sync spidertianye.com

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 // entrace file
-echo 'hello~ DiTi';
 ?>
+<html>
+    <head>
+        <title>DiTi</title>
+    </head>
+    <body>
+        <div style="border: 1px solid red; height: 100px; width: 100px"></div>
+        <div style="border: 1px solid red; height: 200px"></div>
+    </body>     
+</html>
