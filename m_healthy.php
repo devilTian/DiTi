f4723524a55d6b786664043092ce5831db84e862
David Tian <spidertianye@hotmail.com>
2014-11-21 09:00:54 +0800

    [healthy] add db.sql and fetch date using pdo

--- m_healthy.php
+++ m_healthy.php
@@ -6,3 +6,5 @@
  * and open the template in the editor.
  */
 var_dump($_POST);
+$dbh = new PDO('mysql:dbname=diti;host=192.168.1.103;', 'spidertianye', 'root');
+var_dump($dbh->query('SELECT * FROM weight')->fetch(PDO::FETCH_ASSOC));
