f2515e602dad8e6befadb010f56459577ed23033
David Tian <spidertianye@hotmail.com>
2015-07-28 18:20:19 +0800

    Revert "[css] responsive header"
    This reverts commit 7633f8a21ccb06de85279c0a61d264176d2cd110.

--- controllers/journey_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Journey_Controller extends Super_Controller {
-    
-    function hokkaido() {
-        $this->load->view('journey/hokkaido', null, array('notes'  => 123));
-    }
-    
-}
-
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Chongqing');
-$valid = array('resume', 'healthy', 'notes', 'login', 'sentence', 'journey');
+$valid = array('resume', 'healthy', 'notes', 'login', 'sentence');
 
 $defController = 'healthy';
 $defFunction   = 'index';
